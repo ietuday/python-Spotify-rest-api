889aa52b679e777a4d419875951a8e08d3417428
udayaditya singh <udayaditya.singh@gmail.com>
2020-12-18 15:12:19 +0530

    "changes"

---
+++ lastfm-spotify/secrets.py
@@ -0,0 +1,17 @@
+def spotify_user_id():
+    """
+    return spotify user id 
+    """
+    return ''
+
+def spotify_token():
+    """
+    return spotify token
+    """
+    return ''
+
+def last_fm_api_key():
+    """
+    return last fm api key
+    """
+    return ''
